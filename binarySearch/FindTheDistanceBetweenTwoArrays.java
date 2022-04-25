a7a2100998134774bbb360043b7cbb4ade8898c5
chandan578 <93089403+chandan578@users.noreply.github.com>
2022-04-25 09:42:04 +0530

    Add files via upload

---
+++ binarySearch/FindTheDistanceBetweenTwoArrays.java
@@ -0,0 +1,90 @@
+package leetcode.binarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Find the Distance Value Between Two Arrays
+Easy
+
+Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+
+The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+Example 1:
+
+Input: arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2
+Output: 2
+Explanation: 
+For arr1[0]=4 we have: 
+|4-10|=6 > d=2 
+|4-9|=5 > d=2 
+|4-1|=3 > d=2 
+|4-8|=4 > d=2 
+For arr1[1]=5 we have: 
+|5-10|=5 > d=2 
+|5-9|=4 > d=2 
+|5-1|=4 > d=2 
+|5-8|=3 > d=2
+For arr1[2]=8 we have:
+|8-10|=2 <= d=2
+|8-9|=1 <= d=2
+|8-1|=7 > d=2
+|8-8|=0 <= d=2
+Example 2:
+
+Input: arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3
+Output: 2
+Example 3:
+
+Input: arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6
+Output: 1
+ 
+
+Constraints:
+
+1 <= arr1.length, arr2.length <= 500
+-1000 <= arr1[i], arr2[j] <= 1000
+0 <= d <= 100
+*/
+
+public class FindTheDistanceBetweenTwoArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int[] arr1 = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+        int d = sc.nextInt();
+        System.out.println(findTheDistanceVAlue(arr1, arr2, d));
+        sc.close();
+    }
+    private static int findTheDistanceVAlue(int[] arr1, int[] arr2, int d) {
+        Arrays.sort(arr2);
+        int res = 0;
+        for (int i : arr1) {
+            int count = 0;
+            int start = 0, end = arr2.length - 1;
+            while (start <= end) {
+                int mid = start + (end - start)/ 2;
+                if (arr2[mid] == i || Math.abs(arr2[mid] - i) <= d) {
+                    count ++;
+                    break;
+                } else if (arr2[mid] < i) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            if (count == 0) {
+                res ++;
+            }
+        }
+        return res;
+    }
+}
